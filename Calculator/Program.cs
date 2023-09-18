49f2658dbb2cde8e302609478be823d38e991b29
Tuomas Reijonen <hture01@xamk.fi>
2023-09-18 15:36:55 +0300

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä x");
+
+            var x = Console.ReadLine();
+
+            Console.WriteLine("Syötä y");
+
+            var y = Console.ReadLine();
+
+            Console.WriteLine("Minkä operaation haluat suorittaa: ");
+            Console.WriteLine("Saatavilla olevat operaatiot: summa, jako, miinus, kerto");
+
+            //kerto = *
+            //plussa = +
+            //miinus = -
+            //jako = /
+            var operaatio = Console.ReadLine();
+
+            int result = 0;
+
+            if (operaatio == "summa")
+            {
+                result = int.Parse(x) + int.Parse(y);
+            }
+            else if (operaatio == "miinus")
+            {
+                result = int.Parse(x) - int.Parse(y);
+            }
+            else if (operaatio == "jako")
+            {
+                result = int.Parse(x) / int.Parse(y);
+            }
+            else if (operaatio == "kerto")
+            {
+                result = int.Parse(x) * int.Parse(y);
+            }
+
+            Console.WriteLine($"Interpolation example: Result {x} {operaatio} {y} = {result}");
+        }
+
+        //Tehtävä 6: Luo oma funktio, joka laskee tulokset operaation ja annettujen numeroiden peruseteella
+
+        //Tehtävä 7: Lisää jako operaatioon if-lauseeseen tarkistus, että jos jompi kumpi numero on 0 ja ilmoita käyttäjälle virhetilanteesta (nollalla ei voi jakaa)
+       
+        //Tehtävä 8: Lisää if lauseseen myös toinen tapa laskea operaatio. Esimerkiksi summa operaatio voidaan tehdä
+        //antamalla + tai summa teksti ja ohjelma osaa suorittaa operaation
+    }
+}
---
+++ SecondProject/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SecondProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tehtävä 1: Kysy käyttäjältä nimi ja syntymävuosi ja tulosta ne konsoliin
+            Console.WriteLine("Anna nimesi");
+
+            //Tehtävä 2: Kerro käyttäjälle, että kuinka vanha hän on.
+            var currentYear = DateTime.Today.Year;
+
+            //Tehtävä 3: Kerro käyttäjälle, että onko hän täysi-ikäinen
+
+            //Tehtävä 4: Kerro käyttäjälle, että mihin generaatioon hän kuuluu. Muista muuttaa merkkijono numeroksi!
+            //  +---------------------+-----------------------+---------------------+
+            //| Sukupolvi | Yleiset syntymävuodet | Ikä vuonna 2023 |
+            //+---------------------+-----------------------+---------------------+
+            //| Gen Alpha(Alpha) | 2010 - 2025 | 0 - 13 vuotta |
+            //+---------------------+-----------------------+---------------------+
+            //| Gen Z(Z - sukupolvi) | 1995 - 2009 | 14 - 28 vuotta |
+            //+---------------------+-----------------------+---------------------+
+            //| Millennials(Y) | 1980 - 1994 | 29 - 43 vuotta |
+            //+---------------------+-----------------------+---------------------+
+            //| Gen X(X - sukupolvi) | 1965 - 1979 | 44 - 58 vuotta |
+            //+---------------------+-----------------------+---------------------+
+            //| Baby Boomers | 1946 - 1964 | 59 - 77 vuotta |
+            //+---------------------+-----------------------+---------------------+
+
+            //Tehtävä 5: Luo oma funktiot, jokaiselle tehtävälle
+        }
+    }
+}
